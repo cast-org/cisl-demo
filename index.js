212f71ae4d74a2de9ee0fe75f8f2ec65c849883c
bgoldowsky <bgoldowsky@cast.org>
2019-01-25 17:01:09 -0500

    Stub modal that opens for image details.

--- index.js
+++ index.js
@@ -7,4 +7,5 @@ import execGlossary from 'script-loader!./src/js/Glossary';
 import execGlossaryEnactor from 'script-loader!./src/js/GlossaryEnactor';
 import execGlossaryPanel from 'script-loader!./src/js/GlossaryPanel';
 import execGlossarySchema from 'script-loader!./src/js/GlossarySchema';
+import execImages from 'script-loader!./src/js/images.js';
 import r2NavWeb from 'expose-loader?r2NavWeb!@evidentpoint/r2-navigator-web';
---
+++ src/js/images.js
@@ -0,0 +1,7 @@
+// Code for dealing with images - in particular, showing detail modals.
+
+function showImageDetails () {
+    $('#image-info-trigger').CFW_Modal('show');
+}
+
+
