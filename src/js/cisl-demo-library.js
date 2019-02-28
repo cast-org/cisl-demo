6e2e79b7ca4754750dadad0173de20643f843f8e
Alan Harnum <aharnum@ocadu.ca>
2019-02-28 14:02:38 -0500

    Remove /pubs directory, all now served from streamer server.

--- src/js/cisl-demo-library.js
+++ src/js/cisl-demo-library.js
@@ -3,8 +3,8 @@
 (function (fluid) {
     "use strict";
 
-    fluid.defaults("cisl.library.viewer", {
-        gradeNames: ["fluid.viewComponent"],
+    fluid.defaults("cisl.library.display", {
+        gradeNames: ["fluid.component"],
         components: {
             indexLoader: {
                 type: "fluid.resourceLoader",
@@ -13,29 +13,27 @@
                         index: "http://localhost:3000/webpubindex.json"
                     }
                 }
+            },
+            libraryView: {
+                type: "fluid.viewComponent",
+                // must specify
+                container: "",
+                options: {
+                    listeners: {
+                        "{indexLoader}.events.onResourcesLoaded": {
+                            func: "cisl.library.display.appendIndexMarkup",
+                            args: ["{indexLoader}.resources.index.resourceText", "{that}.itemTemplate", "{that}.container"]
+                        }
+                    },
+                    members: {
+                        itemTemplate: '<div class="card card-index"> <div class="card-img"> <img class="card-img-top img-fluid" src="%pubRoot/%id/images/%image" alt="%alt"> </div> <div class="card-body"> <div class="eyebrow">%type</div> <a href="index-readium.html?pub=%id" class="card-title">%title</a> </div>'
+                    }
+                }
             }
-        },
-        listeners: {
-            "{indexLoader}.events.onResourcesLoaded": {
-                func: "cisl.library.viewer.createLibraryIndex",
-                args: ["{indexLoader}.resources.index.resourceText", "{that}.itemTemplate", "{that}.container"]
-            }
-        },
-        members: {
-            itemTemplate: `
-                <div class="card card-index">
-                    <div class="card-img">
-                        <img class="card-img-top img-fluid" src="pubs/%id/images/%image" alt="%alt">
-                    </div>
-                    <div class="card-body">
-                        <div class="eyebrow">%type</div>
-                        <a href="index-readium.html?pub=%id" class="card-title">%title</a>
-                    </div>
-            `
         }
     });
 
-    cisl.library.viewer.createLibraryIndex = function(indexResourceText, itemTemplate, container) {
+    cisl.library.display.appendIndexMarkup = function(indexResourceText, itemTemplate, container) {
         var index = JSON.parse(indexResourceText);
         fluid.each(index, function (item) {
             var itemMarkup = fluid.stringTemplate(itemTemplate, {
@@ -43,7 +41,8 @@
                 image: item.image,
                 alt: item.alt,
                 title: item.title,
-                type: item.type
+                type: item.type,
+                pubRoot: item.pubRoot
             });
             container.append(itemMarkup);
         });
