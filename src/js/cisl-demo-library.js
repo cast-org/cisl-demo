89d56332d7b0b09619d1a6b9734816506baa7983
Alan Harnum <aharnum@ocadu.ca>
2019-03-05 16:58:39 -0500

    Factor out libraryView component.

--- src/js/cisl-demo-library.js
+++ src/js/cisl-demo-library.js
@@ -7,7 +7,7 @@
         gradeNames: "fluid.component",
         invokers: {
             createIndex: {
-                funcName: "cisl.library.display.createIndex",
+                funcName: "cisl.library.libraryIndex.createIndex",
                 args: [
                     "{arguments}.0",
                     "{that}"
@@ -16,9 +16,66 @@
         }
     });
 
+    cisl.library.libraryIndex.createIndex = function (publicationsResourceText, libraryIndexComponent) {
+            var publicationsFeed = JSON.parse(publicationsResourceText);
+
+            var index = {};
+
+            fluid.each(publicationsFeed.publications, function (publication) {
+                // TODO: this is not great
+                var manifestUrl = publication.links[0].href;
+
+                var streamerPubId = cisl.library.libraryIndex.parsePubIdFromUrl(manifestUrl);
+
+                var identifier = publication.metadata.identifier;
+
+                var pubForIndex = $.extend({}, publication, {
+                    streamerPubId: streamerPubId
+                });
+
+                index[identifier] = pubForIndex;
+            });
+
+            libraryIndexComponent.index = index;
+            libraryIndexComponent.events.onIndexReady.fire();
+    };
+
+    cisl.library.libraryIndex.parsePubIdFromUrl = function (pubUrl) {
+        var pubId = pubUrl.split("pub/")[1].split("/manifest")[0];
+        return pubId;
+    };
+
     fluid.defaults("cisl.library.libraryView", {
-        gradeNames: "fluid.viewComponent"
+        gradeNames: "fluid.viewComponent",
+        contentServerRootUrl: "http://localhost:3000",
+        strings: {
+            itemTemplate: '<div class="card card-index"> <div class="card-img"> <img class="card-img-top img-fluid" src="%image" alt="%alt"> </div> <div class="card-body"> <div class="eyebrow">%type</div> <a href="index-readium.html?pub=%streamerPubId&pubDirectory=%pubDirectory" class="card-title">%title</a> </div>'
+        },
+        invokers: {
+            appendIndexMarkup: {
+                funcName: "cisl.library.libraryView.appendIndexMarkup",
+                args: ["{arguments}.0", "{that}.options.strings.itemTemplate", "{that}.options.contentServerRootUrl", "{that}.container"]
+            }
+        }
     });
+
+    cisl.library.libraryView.appendIndexMarkup = function(libraryIndex, itemTemplate, contentServerRootUrl, container) {
+
+        fluid.each(libraryIndex, function (publication) {
+
+            var itemMarkup = fluid.stringTemplate(itemTemplate, {
+                streamerPubId: publication.streamerPubId,
+                // TODO: this is not great
+                image: publication.images[0].href,
+                alt: "",
+                title: publication.metadata.title,
+                type: publication.metadata.description,
+                contentServerRootUrl: contentServerRootUrl,
+                pubDirectory: "pub"
+            });
+            container.append(itemMarkup);
+        });
+    };
 
     fluid.defaults("cisl.library.display", {
         gradeNames: ["fluid.component"],
@@ -46,71 +103,19 @@
                 }
             },
             libraryView: {
-                type: "fluid.viewComponent",
+                type: "cisl.library.libraryView",
                 // must specify
                 container: "",
                 options: {
                     listeners: {
                         "{libraryIndex}.events.onIndexReady": {
-                            func: "cisl.library.display.appendIndexMarkup",
-                            args: ["{libraryIndex}.index", "{that}.options.strings.itemTemplate", "{that}.options.contentServerRootUrl", "{that}.container"]
+                            func: "{that}.appendIndexMarkup",
+                            args: ["{libraryIndex}.index"]
                         }
-                    },
-                    contentServerRootUrl: "http://localhost:3000",
-                    strings: {
-                        itemTemplate: '<div class="card card-index"> <div class="card-img"> <img class="card-img-top img-fluid" src="%image" alt="%alt"> </div> <div class="card-body"> <div class="eyebrow">%type</div> <a href="index-readium.html?pub=%id&pubDirectory=%pubDirectory" class="card-title">%title</a> </div>'
                     }
                 }
             }
         }
     });
-
-    cisl.library.display.createIndex = function (publicationsResourceText, libraryIndexComponent) {
-            var publicationsFeed = JSON.parse(publicationsResourceText);
-
-            var index = {};
-
-            fluid.each(publicationsFeed.publications, function (publication) {
-                // TODO: this is not great
-                var manifestUrl = publication.links[0].href;
-
-                var streamerPubId = cisl.library.display.parsePubIdFromUrl(manifestUrl);
-
-                var identifier = publication.metadata.identifier;
-
-                var pubForIndex = $.extend({}, publication, {
-                    streamerPubId: streamerPubId
-                });
-
-                index[identifier] = pubForIndex;
-            });
-
-            libraryIndexComponent.index = index;
-            libraryIndexComponent.events.onIndexReady.fire();
-    };
-
-    cisl.library.display.appendIndexMarkup = function(libraryIndex, itemTemplate, contentServerRootUrl, container) {
-
-        fluid.each(libraryIndex, function (publication) {
-
-            var itemMarkup = fluid.stringTemplate(itemTemplate, {
-                id: publication.streamerPubId,
-                // TODO: this is not great
-                image: publication.images[0].href,
-                alt: "",
-                title: publication.metadata.title,
-                type: publication.metadata.description,
-                contentServerRootUrl: contentServerRootUrl,
-                pubDirectory: "pub"
-            });
-            container.append(itemMarkup);
-        });
-    };
-
-    cisl.library.display.parsePubIdFromUrl = function (pubUrl) {
-        var pubId = pubUrl.split("pub/")[1].split("/manifest")[0];
-        console.log(pubId);
-        return pubId;
-    };
 
 })(fluid_3_0_0);
